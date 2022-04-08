a72cceee9cc7039a4b46634ee1a5827724b1d60a
GabrieleDiMacio <gabry15dmc@gmail.com>
2022-04-08 12:05:30 +0200

    progettosettimana11

---
+++ src/app/auth/login.page.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Component({
+  template: `
+    <style>
+      :host{
+        display: flex;
+        height: 100%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+      form {
+        max-width: 300px;
+        width: 100%;
+      }
+    </style>
+    <form #form="ngForm" (ngSubmit)="login(form)" >
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>LOGIN</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <div fxLayout>
+            <div>
+              <mat-form-field class="full-width">
+                <input
+                  matInput
+                  placeholder="Email"
+                  type="email"
+                  #email="ngModel"
+                  ngModel
+                  name="email"
+                  required
+                />
+                <mat-error *ngIf="email.invalid">Email Obbligatoria</mat-error>
+              </mat-form-field>
+            </div>
+          </div>
+          <div fxLayout>
+            <div>
+              <mat-form-field class="full-width">
+                <input
+                  matInput
+                  type="password"
+                  #password="ngModel"
+                  placeholder="Password"
+                  ngModel
+                  name="password"
+                  required
+                />
+                <mat-error *ngIf="password.invalid"> Password Obbligatoria</mat-error>
+              </mat-form-field>
+            </div>
+          </div>
+        </mat-card-content>
+        <mat-card-actions>
+          <button mat-raised-button color="primary" type="submit">Accedi</button>
+        </mat-card-actions>
+      </mat-card>
+    </form>
+    <div>
+      Oppure<a [routerLink]="['/register']"   style="color: white;"><br> Registrati</a>
+    </div>
+  `,
+  styles: [],
+})
+export class LoginPage implements OnInit {
+  constructor(private authSrv: AuthService, private router:Router) {}
+
+  ngOnInit(): void {}
+
+  async login(form: NgForm) {
+    try {
+      await this.authSrv.login(form.value).toPromise();
+      this.router.navigate(['/'])
+    } catch (error) {
+      alert('error')
+    }
+  }
+}
---
+++ src/app/features/dashboard/nav/nav.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  template: `
+    <mat-sidenav-container class="sidenav-container">
+      <mat-sidenav #drawer class="sidenav" fixedInViewport
+          [attr.role]="(isHandset$ | async) ? 'dialog' : 'navigation'"
+          [mode]="(isHandset$ | async) ? 'over' : 'side'"
+          [opened]="(isHandset$ | async) === false">
+        <mat-toolbar>MENU</mat-toolbar>
+        <mat-nav-list>
+          <a mat-list-item [routerLink]="['/']" routerLinkActive="router-link-active">MOVIES</a>
+          <a mat-list-item [routerLink]="['/profile']" routerLinkActive="router-link-active">PROFILE</a>
+          <a mat-list-item class="logout" (click)="logout()" routerLinkActive="router-link-active">ESCI</a>
+        </mat-nav-list>
+      </mat-sidenav>
+      <mat-sidenav-content>
+        <mat-toolbar color="primary">
+          <button
+            type="button"
+            aria-label="Toggle sidenav"
+            mat-icon-button
+            (click)="drawer.toggle()"
+            *ngIf="isHandset$ | async">
+            <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
+          </button>
+          <span>Movies</span>
+        </mat-toolbar>
+        <ng-content ></ng-content>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+
+  `,
+  styles: [`
+    .sidenav-container {
+      height: 100%;
+    }
+
+    .sidenav {
+      width: 200px;
+    }
+
+    .sidenav .mat-toolbar {
+      background: inherit;
+    }
+
+    .mat-toolbar.mat-primary {
+      position: sticky;
+      top: 0;
+      z-index: 1;
+    }
+    .logout{
+      position:absolute;
+      bottom:20px
+    }
+
+  `]
+})
+export class NavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private auth:AuthService) {}
+
+  logout(){
+    this.auth.logout()
+  }
+}
